0bb1de3ae9fe1a0869ec074d33e73ebba39836c0
Claire <eaaclhb@students.eaaa.dk>
2023-10-28 00:19:41 +0200

    Logout - home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,29 @@
+import { getAuth, signOut } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+
 export default function Home() {
-  return <div>Home</div>;
+  const auth = getAuth();
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        Navigate("/");
+        console.log("Signed out successfully");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.error("Error signing out", error);
+        console.log("Error signing out");
+      });
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
 }
