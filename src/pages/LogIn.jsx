463ff9931966230dca0db9119a87f53b3840a971
Claire <eaaclhb@students.eaaa.dk>
2023-10-27 23:57:58 +0200

    Update LogIn.jsx

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,3 +1,65 @@
-export default function LogIn() {
-  return <div>LogIn</div>;
+import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+import { NavLink, useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/home");
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="email-address">Email address</label>
+          <input
+            id="email-address"
+            name="email"
+            type="email"
+            required
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button onClick={onLogin}>Login</button>
+        </div>
+      </form>
+
+      <p>
+        No account yet? <NavLink to="/signup">Sign up</NavLink>
+      </p>
+    </div>
+  );
 }
