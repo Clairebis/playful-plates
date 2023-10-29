cbcc9dc7570a67e4d98a12e13e03f23c16f49b95
Claire <eaaclhb@students.eaaa.dk>
2023-10-29 13:25:59 +0100

    Update Home.jsx
    User welcome message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,33 @@
 import { getAuth, signOut } from "firebase/auth";
-import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const auth = getAuth();
+  const navigate = useNavigate();
+  const uid = auth.currentUser.uid;
+  const [username, setUsername] = useState("");
+
+  const url = `https://playful-plates-b4a84-default-rtdb.europe-west1.firebasedatabase.app/users/${uid}.json`;
+
+  useEffect(() => {
+    async function getUser() {
+      const response = await fetch(url);
+      const userData = await response.json();
+
+      if (userData) {
+        //if userData exists, set states with values from userData (firebase)
+        setUsername(userData.username);
+      }
+    }
+    getUser();
+  }, [auth.currentUser, url]); // dependencies: useEffect is executed when auth.currentUser changes
 
   const handleLogout = () => {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
-        Navigate("/");
+        navigate("/");
         console.log("Signed out successfully");
       })
       .catch((error) => {
@@ -21,6 +40,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <p>Hello {username}</p>
       <div>
         <button onClick={handleLogout}>Logout</button>
       </div>
