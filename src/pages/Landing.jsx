1c2440276d61e24cf8f6d5e15df5702b59eab7dc
Siiriellisylvia <113165765+Siiriellisylvia@users.noreply.github.com>
2023-10-30 22:38:20 +0100

    Structure

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,8 +5,9 @@ export default function Landing() {
   return (
     <div>
       <h1>Landing page</h1>
-      <Button text="Login" Link="/login"/>
-      <Button text="Sign up" Link="/signup"/>
+      <Button/>
+      <Link to="/login">Log In</Link>
+      <Link to="/signup">Sign Up</Link>
     </div>
   );
 }
