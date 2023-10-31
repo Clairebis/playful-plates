9baadb8174f0661e0abbaac1017bcfc378774eec
nsoktab <nataliasokolowskaa@gmail.com>
2023-10-31 12:13:02 +0100

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+//-----------Natalia-------------//
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
+import arrow from "./arrowback.svg";
+
+export default function Header({ pageTitle }) {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1); // This will navigate one page back
+  };
+
+  return (
+    <div className="app-header">
+      <div className="arrow" onClick={goBack}>
+        <img src={arrow} alt="" />
+      </div>
+      <div className="page-title">{pageTitle}</div>
+    </div>
+  );
+}
