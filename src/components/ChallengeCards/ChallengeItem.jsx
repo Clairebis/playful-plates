99a179d56ba32c4bc74d552339eda76708c2c408
Siiriellisylvia <113165765+Siiriellisylvia@users.noreply.github.com>
2023-11-01 21:09:31 +0100

    styling

--- src/components/ChallengeCards/ChallengeItem.jsx
+++ src/components/ChallengeCards/ChallengeItem.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import "./Challengecards.css"
+import chefHatIconWhite from "../../assets/Icons/chefHatIconWhite.svg"
 
 export default function ChallengeItem({ post }) {
     const navigate = useNavigate();
@@ -10,13 +11,27 @@ export default function ChallengeItem({ post }) {
 
     return (
         <>
-        <article className="challenge-card" key={post.id} onClick={handleClick}>
-            <img className="challenge-image"src={post.image} alt={post.title} />
-            <div>
-                <h2>{post.title}</h2>
-                <p>{post.subheading}</p>
+        <div className="challenge-card-container">
+            <div className="challenge-card" key={post.id} onClick={handleClick}>
+                <img className="challenge-image"src={post.image} alt={post.title} />
+                <div>
+                    <h2>{post.title}</h2>
+                    <p>{post.subheading}</p>
+                </div>
             </div>
-        </article>
+
+            <div className="challenge-card-bottom">
+                <p>2 friends joined</p>
+                <div className="challengeCardCompleted">
+                    <img
+                        src={chefHatIconWhite}
+                        alt="chef hat icon"
+                    />
+                    <p>20 completed</p>
+                </div>
+            </div>
+            
+        </div>
         </>
     );
     }
