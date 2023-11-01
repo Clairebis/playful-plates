a2cb30bf8e33c8793ec346c23a3fc52fa2faa529
Claire <eaaclhb@students.eaaa.dk>
2023-11-01 10:29:04 +0100

    css

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -13,20 +13,20 @@ export default function PostCard({ post }) {
   }
 
   return (
-    <section className="container">
+    <section className="postCardContainer">
       <article className="postCard" onClick={openPost}>
         <img src={post.image} alt={post.title} className="postCardImage" />
         <section className="postCardLower">
           <PostCardUser uid={post.uid} />
-          <p className="bold">{post.title}</p>
+          <p className="postCardBold">{post.title}</p>
           <p className="postCardDate">{post.publishedAt}</p>
         </section>
       </article>
-      <div className="likes">
-        <div className="heart">
+      <div className="postCardLikes">
+        <div className="postCardHeart">
           <Heart isActive={active} onClick={() => setActive(!active)} />
         </div>
-        <div className="likeText"> {post.likes} likes </div>
+        <div className="postCardLikeText"> {post.likes} likes </div>
       </div>
     </section>
   );
