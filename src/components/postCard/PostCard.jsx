0116fc45c41a372d2017af43d032468f330b95c1
Claire <eaaclhb@students.eaaa.dk>
2023-11-05 21:29:26 +0100

    working on likes

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -7,9 +7,21 @@ import "./postCard.css";
 export default function PostCard({ post }) {
   const navigate = useNavigate();
   const [active, setActive] = useState(false);
+  const [likeCount, setLikeCount] = useState(post.likes);
 
   function openPost() {
     navigate(`/post/${post.id}`);
+  }
+
+  function handleLikeClick() {
+    setActive(!active);
+
+    // Update the like count based on the active state (not connected to db...)
+    if (active) {
+      setLikeCount(likeCount - 1);
+    } else {
+      setLikeCount(likeCount + 1);
+    }
   }
 
   return (
@@ -18,15 +30,15 @@ export default function PostCard({ post }) {
         <img src={post.image} alt={post.title} className="postCardImage" />
         <section className="postCardLower">
           <PostCardUser uid={post.uid} />
-          <p className="postCardBold">{post.title}</p>
+          <p className="postCardBoldTitle">{post.title}</p>
           <p className="postCardDate">{post.publishedAt}</p>
         </section>
       </article>
       <div className="postCardLikes">
         <div className="postCardHeart">
-          <Heart isActive={active} onClick={() => setActive(!active)} />
+          <Heart isActive={active} onClick={handleLikeClick} />
         </div>
-        <div className="postCardLikeText"> {post.likes} likes </div>
+        <div className="postCardLikeText"> {likeCount} likes </div>
       </div>
     </section>
   );
