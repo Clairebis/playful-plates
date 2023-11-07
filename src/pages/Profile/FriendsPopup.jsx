d920202a99f108c40ee12476d7fcde5ca085b375
Burbele <eaapbur@students.eaaa.dk>
2023-11-07 20:36:57 +0100

    Friends styled

--- src/pages/Profile/FriendsPopup.jsx
+++ src/pages/Profile/FriendsPopup.jsx
@@ -1,12 +1,165 @@
+// import close from "../../Assets/Icons/close.svg";
+
+// import "./FriendsPopup.css";
+
+// function FriendsPopup({ isVisible, onClose }) {
+//   if (!isVisible) return null;
+
+//   return (
+//     <div className="friends-popup">
+//       <div className="popup-content">
+// <div className="popup-header">
+//   <h1>Send a friend request</h1>
+//   <p>Send a friend request by typing a username</p>
+//   <img
+//     src={close}
+//     alt="Close"
+//     className="close-button"
+//     onClick={onClose}
+//   />
+// </div>
+//         <div className="input-container">
+//           <input
+//             type="text"
+//             placeholder="Username"
+//             className="username-input"
+//           />
+//           <button className="invite-button">Invite</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default FriendsPopup;
+
+// import { useState } from "react";
+// import { getDatabase, ref, query, orderByChild, equalTo, get } from "firebase/database";
+// import close from "../../Assets/Icons/close.svg";
+
+// import "./FriendsPopup.css";
+
+// function FriendsPopup({ isVisible, onClose, updateUserData }) {
+//   const [username, setUsername] = useState("");
+//   const [message, setMessage] = useState("");
+
+//   const handleUsernameChange = (event) => {
+//     setUsername(event.target.value);
+//   };
+
+//   const handleFetchUserInfo = async () => {
+//     const db = getDatabase();
+//     const usersRef = ref(db, "users");
+
+//     const userQuery = query(usersRef, orderByChild("username"), equalTo(username));
+
+//     try {
+//       const snapshot = await get(userQuery);
+//       if (snapshot.exists()) {
+//         const userData = snapshot.val();
+//         const userDataKeys = Object.keys(userData);
+//         if (userDataKeys.length === 1) {
+//           const userUid = userDataKeys[0];
+//           const user = userData[userUid];
+//           updateUserData(user);
+//           setMessage(`User found: ${user.username}`);
+//         } else {
+//           updateUserData(null);
+//           setMessage("User not found. Please try again.");
+//         }
+//       } else {
+//         updateUserData(null);
+//         setMessage("User not found. Please try again.");
+//       }
+//     } catch (error) {
+//       console.error("Error fetching user data", error);
+//     }
+//   };
+
+//   return (
+//     <div className={`friends-popup ${isVisible ? "visible" : ""}`}>
+//       <div className="popup-content">
+//         <div className="popup-header">
+//           <h1>Send a friend request</h1>
+//           <p>Send a friend request by typing a username</p>
+//           <img
+//             src={close}
+//             alt="Close"
+//             className="close-button"
+//             onClick={() => {
+//               console.log("Close button clicked"); // Add this line for debugging
+//               onClose();
+//             }}
+//           />
+//           <input
+//             type="text"
+//             placeholder="Username"
+//             className="username-input"
+//             value={username}
+//             onChange={handleUsernameChange}
+//           />
+//           <button
+//             className="invite-button"
+//             onClick={handleFetchUserInfo}>
+//             Invite
+//           </button>
+//           {message && <p>{message}</p>}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default FriendsPopup;
+
+import { useState } from "react";
+import { getDatabase, ref, query, orderByChild, equalTo, get } from "firebase/database";
 import close from "../../Assets/Icons/close.svg";
 
 import "./FriendsPopup.css";
 
-function FriendsPopup({ isVisible, onClose }) {
-  if (!isVisible) return null;
+function FriendsPopup({ isVisible, onClose, updateUserData }) {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
 
-  return (
-    <div className="friends-popup">
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleFetchUserInfo = async () => {
+    const db = getDatabase();
+    const usersRef = ref(db, "users");
+
+    const userQuery = query(usersRef, orderByChild("username"), equalTo(username));
+
+    try {
+      const snapshot = await get(userQuery);
+      if (snapshot.exists()) {
+        const userData = snapshot.val();
+        const userDataKeys = Object.keys(userData);
+        if (userDataKeys.length === 1) {
+          const userUid = userDataKeys[0];
+          const user = userData[userUid];
+          updateUserData(user);
+          setMessage(`User found: ${user.username}`);
+
+          // Save the friend data to local storage
+          localStorage.setItem("myFriendsData", JSON.stringify(user));
+        } else {
+          updateUserData(null);
+          setMessage("User not found. Please try again.");
+        }
+      } else {
+        updateUserData(null);
+        setMessage("User not found. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error fetching user data", error);
+    }
+  };
+
+  return isVisible ? (
+    <div className="friends-popup visible">
       <div className="popup-content">
         <div className="popup-header">
           <h1>Send a friend request</h1>
@@ -15,20 +168,25 @@ function FriendsPopup({ isVisible, onClose }) {
             src={close}
             alt="Close"
             className="close-button"
-            onClick={onClose}
+            onClick={onClose} // Call the onClose function to close the popup
           />
         </div>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Username"
-            className="username-input"
-          />
-          <button className="invite-button">Invite</button>
-        </div>
+        <input
+          type="text"
+          placeholder="Username"
+          className="username-input"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <button
+          className="invite-button"
+          onClick={handleFetchUserInfo}>
+          Invite
+        </button>
+        {message && <p>{message}</p>}
       </div>
     </div>
-  );
+  ) : null;
 }
 
 export default FriendsPopup;
--- src/pages/Profile/MyFriends.jsx
+++ src/pages/Profile/MyFriends.jsx
@@ -1,12 +1,131 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import Header from "../../components/Header/Header";
+// import addImage from "../../Assets/Icons/add.svg";
+// import FriendsPopup from "../../pages/Profile/FriendsPopup"; // Import the FriendsPopup component
+
+// import "./MyFriends.css";
+
+// function MyFriends() {
+//   const [isPopupVisible, setPopupVisible] = useState(false);
+
+//   const openPopup = () => {
+//     setPopupVisible(true);
+//   };
+
+//   const closePopup = () => {
+//     setPopupVisible(false);
+//   };
+
+//   return (
+//     <>
+//       <section className="page">
+//         <Header pageTitle="My Friends" />
+//         <div className="button-container">
+//           <button
+//             className="add-friend-button"
+//             onClick={openPopup}>
+//             <img
+//               src={addImage}
+//               alt="Add"
+//             />
+//             Add
+//           </button>
+//         </div>
+//       </section>
+//       <FriendsPopup
+//         isVisible={isPopupVisible}
+//         onClose={closePopup}
+//       />{" "}
+//       {/* Render the FriendsPopup component */}
+//     </>
+//   );
+// }
+
+// export default MyFriends;
+
+// import { useState, useEffect } from "react";
+// import Header from "../../components/Header/Header";
+// import addImage from "../../Assets/Icons/add.svg";
+// import FriendsPopup from "../../pages/Profile/FriendsPopup";
+
+// import "./MyFriends.css";
+
+// function MyFriends() {
+//   const [isPopupVisible, setPopupVisible] = useState(false);
+//   const [userData, setUserData] = useState(null);
+
+//   const openPopup = () => {
+//     setPopupVisible(true);
+//   };
+
+//   const closePopup = () => {
+//     setPopupVisible(false);
+//   };
+
+//   const updateUserData = (data) => {
+//     setUserData(data);
+//   };
+
+//   return (
+//     <>
+//       <section className="page">
+//         <Header pageTitle="My Friends" />
+//         <div className="button-container">
+//           <button
+//             className="add-friend-button"
+//             onClick={openPopup}>
+//             <img
+//               src={addImage}
+//               alt="Add"
+//             />
+//             Add
+//           </button>
+//         </div>
+//       </section>
+//       <FriendsPopup
+//         isVisible={isPopupVisible}
+//         onClose={closePopup} // Pass the closePopup function
+//         updateUserData={updateUserData}
+//       />
+//       <div className="user-info">
+//         {userData && (
+//           <div>
+//             <img
+//               src={userData.image}
+//               alt="User"
+//             />
+//             <p>{userData.name}</p>
+//             <p>{userData.username}</p>
+//             <p>{userData.level}</p>
+//             <p>{userData.xp}</p>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default MyFriends;
+
+import { useState, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import addImage from "../../Assets/Icons/add.svg";
-import FriendsPopup from "../../pages/Profile/FriendsPopup"; // Import the FriendsPopup component
+import moreImage from "../../Assets/Icons/more.svg";
+import FriendsPopup from "../../pages/Profile/FriendsPopup";
 
 import "./MyFriends.css";
 
 function MyFriends() {
   const [isPopupVisible, setPopupVisible] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  // Retrieve friend data from local storage when the component loads
+  useEffect(() => {
+    const storedFriendData = localStorage.getItem("myFriendsData");
+    if (storedFriendData) {
+      setUserData(JSON.parse(storedFriendData));
+    }
+  }, []);
 
   const openPopup = () => {
     setPopupVisible(true);
@@ -14,6 +133,10 @@ function MyFriends() {
 
   const closePopup = () => {
     setPopupVisible(false);
+  };
+
+  const updateUserData = (data) => {
+    setUserData(data);
   };
 
   return (
@@ -31,12 +154,39 @@ function MyFriends() {
             Add
           </button>
         </div>
+
+        <FriendsPopup
+          isVisible={isPopupVisible}
+          onClose={closePopup} // Pass the closePopup function
+          updateUserData={updateUserData}
+        />
+        <div className="friends-friends-container">
+          {userData && (
+            <div className="friends-user-info">
+              <div className="container-flex">
+                <img
+                  src={userData.image}
+                  alt="User"
+                  className="friend-user-image"
+                />
+                <div className="friend-user-details">
+                  <p className="friend-user-name">{userData.name}</p>
+                  <p className="friend-user-username">{userData.username}</p>
+                  <p className="friend-user-level">{userData.level}</p>
+                  <p className="friend-user-xp">{userData.xp} XP</p>
+                </div>
+              </div>
+              <div className="more-icon">
+                <img
+                  src={moreImage}
+                  alt="More"
+                  className="more-image"
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </section>
-      <FriendsPopup
-        isVisible={isPopupVisible}
-        onClose={closePopup}
-      />{" "}
-      {/* Render the FriendsPopup component */}
     </>
   );
 }
