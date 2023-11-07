e42a77f343da2eb3c86a39d5673224f6ed934332
Siiriellisylvia <113165765+Siiriellisylvia@users.noreply.github.com>
2023-11-07 20:47:33 +0100

    Update HomeXPButton.jsx

--- src/components/HomeHeader/HomeXPButton.jsx
+++ src/components/HomeHeader/HomeXPButton.jsx
@@ -1,16 +1,37 @@
 import chefHatIcon from "../../assets/Icons/chefHatIcon.svg"
 import { Link } from "react-router-dom";
+import LevelInfoPopup from "../../pages/Profile/LevelInfoPopup";
+import { useState } from "react";
 
 export default function HomeXpPoints () {
 
+    const [isPopupVisible, setPopupVisible] = useState(false);
+
+    const handleClick = () => {
+      setPopupVisible(true);
+    };
+  
+    const closePopup = () => {
+      setPopupVisible(false);
+    };
+
     return (
 
-    <Link className="homeXpPoints" to="/*">
+    <div className="homeXpPoints" onClick={handleClick}>
         <img
             src={chefHatIcon}
             alt="chef hat icon"
         />
         <p>250 XP</p>
-    </Link>
+
+        {isPopupVisible && (
+        <LevelInfoPopup
+          isVisible={isPopupVisible}
+          onClose={closePopup}
+        />
+      )}
+    </div>
+
+    
     )
 }
